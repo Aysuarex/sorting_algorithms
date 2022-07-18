44c901097089785809be99582db5f9f4cbc56b08
root <root@u-station>
2022-07-18 20:10:19 +0000

    0-bubble_sort.c updated

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,23 +11,25 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, k;
-	int tmp;
+	size_t i;
+	int tmp, sorted;
 
 	if (!array || size == 0)
 		return;
 
-	for (k = 0; k < size - 1; k++)
-	{
+	do {
+		sorted = 1;
 		for (i = 0; i < size - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
+				sorted = 0;
 				tmp = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = tmp;
 				print_array(array, size);
 			}
 		}
-	}
+
+	} while (sorted == 0);
 }
